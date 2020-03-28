cd22ebdcb1d2047bf0d0ed78d59f98587c2d6d02
ys270@duke.edu <ys270@duke.edu>
2020-03-28 15:58:20 -0400

    django_version

---
+++ mydb/main.py
@@ -0,0 +1,9 @@
+import create_database
+import queries
+
+def main():
+    create_database.main()
+    queries.main()
+    
+if __name__=="__main__":
+    main()
---
+++ mydb/queries.py
@@ -0,0 +1,83 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mydb.settings")
+
+import django
+django.setup()
+
+from query.models import State, Color, Team, Player
+
+def add_state(name):
+    State.objects.get_or_create(name=name)
+
+def add_color(name):
+    Color.objects.get_or_create(name=name)
+
+def add_team(name,state_id,color_id,wins,losses):
+    Team.objects.get_or_create(name=name,state_id = State.objects.get(state_id = state_id), color_id = Color.objects.get(color_id = color_id),wins=wins,losses=losses)
+
+def add_player(team_id,uniform_num,first_name,last_name,mpg,ppg,rpg,apg,spg,bpg):
+    Player.objects.get_or_create(team_id=Team.objects.get(team_id = team_id),uniform_num=uniform_num,first_name=first_name,last_name=last_name,mpg=mpg,ppg=ppg,rpg=rpg,apg=apg,spg=spg,bpg=bpg)
+
+def query1(use_mpg,min_mpg,max_mpg,use_ppg,min_ppg,max_ppg,use_rpg,min_rpg,max_rpg,use_apg,min_apg,max_apg,use_spg,min_spg,max_spg,use_bpg,min_bpg,max_bpg):
+    res = Player.objects.all()
+    if use_mpg:
+        res = res.filter(mpg__gte = min_mpg, mpg__lte = max_mpg)
+    if use_ppg:
+        res = res.filter(ppg__gte = min_ppg, ppg__lte = max_ppg)
+    if use_rpg:
+        res = res.filter(rpg__gte = min_rpg, rpg__lte = max_rpg)
+    if use_apg:
+        res = res.filter(apg__gte = min_apg, apg__lte = max_apg)
+    if use_spg:
+        res = res.filter(spg__gte = min_spf, spg__lte = max_spg)
+    if use_bpg:
+        res = res.filter(bpg__gte = min_bpg, bpg__lte = max_bpg)
+    print("PLAYER_ID TEAM_ID UNIFORM_NUM FIRST_NAME LAST_NAME MPG PPG RPG APG SPG BPG")
+    for player in res:
+        print(player)
+    return
+
+def query2(color_name):
+    res = Team.objects.filter(color_id__name = color_name)
+    print("NAME")
+    for team in res:
+        print(team.name)
+    return
+
+def query3(team_name):
+    res = Player.objects.filter(team_id__name = team_name).order_by('-ppg')
+    print("FIRST_NAME LAST_NAME")
+    for player in res:
+        print(player.first_name,player.last_name)
+    return
+
+def query4(state_name,color_name):
+    res = Player.objects.filter(team_id__state_id__name = state_name, team_id__color_id__name = color_name)
+    print("FIRST_NAME LAST_NAME UNIFORM_NUM")
+    for player in res:
+        print(player.first_name,player.last_name,player.uniform_num)
+    return 
+
+def query5(wins_num):
+    res = Player.objects.filter(team_id__wins__gt = wins_num)
+    print("FIRST_NAME LAST_NAME NAME WINS")
+    for player in res:
+        print(player.first_name,player.last_name,player.team_id.name,player.team_id.wins)
+    return
+
+def main():
+    query1(1, 35, 40, 0, 0, 0, 0, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    query1(0, 35, 40, 0, 0, 0, 1, 5, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    query2("Green")
+    query2("Orange")
+    query3("FloridaState")
+    query3("Duke")
+    query4("NC", "Red")
+    query4("NC", "DarkBlue")
+    query5(10)
+    query5(13)
+    return
+
+if __name__ == "__main__":
+    main()
+
